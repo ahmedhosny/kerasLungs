3f8d18f195d770b368281336731a29d53f275682
Ahmed Hosny <i.ahmedhosny@gmail.com>
2017-03-31 18:09:27 -0400

    #49

--- script.py
+++ script.py
@@ -20,7 +20,7 @@ from keras import regularizers
 
 
 # current version
-RUN = "48" 
+RUN = "49" 
 
 # you want 2d or 3d convolutions?
 mode = "2d"
@@ -50,7 +50,7 @@ regul = regularizers.l2(0.0000001)
 # others...
 batch_size = 32 
 nb_epoch = 300000
-lr = 0.00001 
+lr = 0.000001 
 
 # print 
 print ("training : run: " , RUN , " lr: " , lr)
